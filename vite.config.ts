4b0658ecf28fc6b183f81a2cec6953e5c6923e77
dert1129 <nathancreger1111@gmail.com>
2026-01-08 13:25:29 -0500

    edit config, edit .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+  },
+  build: {
+    outDir: "./build",
+  },
+  preview: {
+    open: true,
+    strictPort: true,
+    port: 3000,
+  }
 })
