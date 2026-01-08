5004366ea185436eaed542b3b714a45282b7cc98
dert1129 <nathancreger1111@gmail.com>
2026-01-08 15:57:08 -0500

    use bootstraps JS and change base

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 // import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import 'bootstrap-5-css-only/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
